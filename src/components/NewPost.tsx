4d5ad238a20103a678793ce57a1c3ae10c19c339
akulv <akulv@umich.edu>
2020-07-14 18:57:01 -0400

    Create post final draft

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import InputBase from "@material-ui/core/InputBase";
 import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import MenuIcon from "@material-ui/icons/Menu";
 import SendIcon from "@material-ui/icons/Send";
 import { normalize } from "path";
@@ -14,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     alignItems: "center",
     width: 914,
-    height: 70,
+    height: 160,
   },
   input: {
     marginLeft: theme.spacing(2),
@@ -28,17 +30,51 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CustomizedInputBase() {
   const classes = useStyles();
+  const [value, setValue] = React.useState("");
+
+  const handleChange = (event: any) => {
+    setValue(event.target.value);
+  };
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Paper component="form" className={classes.root}>
-      <IconButton className={classes.iconButton} aria-label="menu">
+      <IconButton
+        className={classes.iconButton}
+        aria-label="menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
         <MenuIcon />
       </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>EECS 183</MenuItem>
+        <MenuItem onClick={handleClose}>BIO 172</MenuItem>
+        <MenuItem onClick={handleClose}>ENGR 100</MenuItem>
+      </Menu>
       <InputBase
         className={classes.input}
         placeholder="Type a post..."
         inputProps={{ "aria-label": "post to feed" }}
         multiline={true}
+        rowsMax={7}
+        value={value}
+        onChange={handleChange}
       />
       <IconButton
         type="submit"
