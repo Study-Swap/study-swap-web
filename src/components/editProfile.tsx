432b542abba586524cdf17fec7dcf1f2b30c55a4
ashishmahuli <ashish.mahuli@gmail.com>
2020-07-19 23:21:37 -0400

    profile errors

---
+++ src/components/editProfile.tsx
@@ -0,0 +1,159 @@
+// eslint-disable-next-line
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../constants/UserContext";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Paper";
+import Avatar from "@material-ui/core/Avatar";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import IconButton from "@material-ui/core/IconButton";
+import CancelIcon from "@material-ui/icons/Cancel";
+import SaveIcon from "@material-ui/icons/Save";
+import { makeStyles } from "@material-ui/core/styles";
+import { userModel } from "../constants/Models";
+import { dummyUser } from "../DummyData/profile";
+
+// eslint-disable-next-line
+import history from "../utils/historyUtils";
+import { logoutUser } from "../utils/firebaseUtils";
+
+const useStyles = makeStyles({
+  root: {
+    width: "80%",
+  },
+
+  media: {
+    height: "100%",
+    width: "100%",
+  },
+  inputBio: {
+    //marginLeft: theme.spacing(2),
+    flex: 1,
+    overflow: "auto",
+    fontSize: 14,
+    width: "100%",
+  },
+  inputName: {
+    //marginLeft: theme.spacing(2),
+    flex: 1,
+    overflow: "auto",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  inputGrade: {
+    //marginLeft: theme.spacing(2),
+    flex: 1,
+    overflow: "auto",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
+export default function editProfile(props: any) {
+  // eslint-disable-next-line
+
+  const classes = useStyles();
+
+  const [bioInput, setBioInput] = useState(props.bio);
+  const [firstInput, setFirstInput] = useState(props.firstName);
+  const [lastInput, setLastInput] = useState(props.lastName);
+  const [gradeInput, setGradeInput] = useState(props.grade);
+
+  return (
+    <Container component="main" maxWidth="md">
+      <Card className={classes.root}>
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xs={2}>
+              <Avatar
+                className={classes.media}
+                alt="Prof Pic"
+                src={require("../components/apoorv.png")}
+              />
+            </Grid>
+
+            <Grid item xs={5}>
+              <div>
+                <OutlinedInput
+                  margin="dense"
+                  className={classes.inputName}
+                  placeholder="First Name"
+                  inputProps={{ "aria-label": "post to feed" }}
+                  value={firstInput}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setFirstInput(event.target.value);
+                  }}
+                />
+                <OutlinedInput
+                  margin="dense"
+                  className={classes.inputName}
+                  placeholder="First Name"
+                  inputProps={{ "aria-label": "post to feed" }}
+                  value={lastInput}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setLastInput(event.target.value);
+                  }}
+                />
+                <OutlinedInput
+                  margin="dense"
+                  className={classes.inputName}
+                  placeholder="First Name"
+                  inputProps={{ "aria-label": "post to feed" }}
+                  value={gradeInput}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setGradeInput(event.target.value);
+                  }}
+                />
+              </div>
+            </Grid>
+            <Grid item>
+              <IconButton
+                type="submit"
+                aria-label="edit"
+                onClick={props.handleCancel}
+              >
+                <CancelIcon />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <IconButton
+                type="submit"
+                aria-label="edit"
+                onClick={props.handleSave({
+                  id: "12",
+                  firstName: firstInput,
+                  lastName: lastInput,
+                  bio: bioInput,
+                  classes: ["EECS 281", "EECS 376"],
+                  grade: gradeInput,
+                  chats: ["Chat stuff"],
+                  signedUp: true,
+                })}
+              >
+                <SaveIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+          <br></br>
+
+          <OutlinedInput
+            margin="dense"
+            className={classes.inputBio}
+            placeholder="Type a Comment..."
+            inputProps={{ "aria-label": "post to feed" }}
+            multiline={true}
+            rowsMax={7}
+            value={bioInput}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setBioInput(event.target.value);
+            }}
+          />
+        </CardContent>
+      </Card>
+    </Container>
+  );
+}
