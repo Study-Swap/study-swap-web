05d248b7b34099ab17f71bf6337e37a763c29775
cmodi <cmodi@umich.edu>
2020-08-11 22:13:39 -0400

    added  bare class page

---
+++ src/views/Class.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Container from "@material-ui/core/Container";
+
+export default function Class() {
+  return (
+    <Container component="main" maxWidth="md">
+      Class
+    </Container>
+  );
+}
