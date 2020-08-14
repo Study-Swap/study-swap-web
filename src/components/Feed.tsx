9d1930684284694c61c09f0d6c0750d14a6b3346
ashishmahuli <MAHULA@pfizer.com>
2020-08-14 00:27:17 -0400

    added working filter box, not connected

---
+++ src/components/Feed.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+// eslint-disable-next-line
+import { UserContext } from "../constants/UserContext";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import NewPost from "../components/NewPost";
+import FeedItem from "../components/FeedItem";
+import { postData } from "../DummyData/home";
+import { getPosts, getFeed, addPost } from "../utils/firebaseUtils";
+import { postModel } from "../constants/Models";
+
+// eslint-disable-next-line
+import history from "../utils/historyUtils";
+
+const useStyles = makeStyles({
+  root: {
+    flex: 1,
+  },
+
+  control: {
+    padding: 2,
+  },
+});
+
+export default function Feed() {
+  const classes = useStyles();
+  // eslint-disable-next-line
+  const [postState, setPostState] = useState<any[]>([]);
+
+  useEffect(() => {
+    console.log(postState);
+    getPosts("1") // classId is hardcoded for now
+      .then((res) => {
+        setPostState(res);
+      })
+      .catch((err) => console.error(err));
+  }, []); // TODO: Add loading indicator and put "refresh" into empty array
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={6}
+      //className={classes.root}
+      style={{ overflow: "auto" }}
+    >
+      <Grid item>
+        <NewPost
+          onClick={(post: postModel) => {
+            addPost(post.userId, post.classId, post).then(
+              (resultingId: any) => {
+                post.id = resultingId;
+              }
+            );
+            setPostState([post, ...postState]);
+          }}
+        />
+      </Grid>
+      {postState.map((thisPost, index) => (
+        <Grid item key={thisPost.id} style={{ width: "500px" }}>
+          <FeedItem
+            id={thisPost.id}
+            postUserName={thisPost.postUserName}
+            postClassName={thisPost.postClassName}
+            postText={thisPost.postText}
+            timestamp={thisPost.timestamp}
+            edited={thisPost.edited}
+            userId={thisPost.userId}
+            classId={thisPost.classId}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,77 +4,139 @@ import { UserContext } from "../constants/UserContext";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import NewPost from "../components/NewPost";
-import FeedItem from "../components/FeedItem";
-import { postData } from "../DummyData/home";
-import { getPosts, getFeed, addPost } from "../utils/firebaseUtils";
-import { postModel } from "../constants/Models";
+import Feed from "../components/Feed";
 
 // eslint-disable-next-line
 import history from "../utils/historyUtils";
+import CardContent from "@material-ui/core/CardContent";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import Typography from "@material-ui/core/Typography";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Checkbox from "@material-ui/core/Checkbox";
+import CircleCheckedFilled from "@material-ui/icons/CheckCircle";
+import CircleUnchecked from "@material-ui/icons/RadioButtonUnchecked";
+import { Paper } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
-    flex: 1,
+    //flex: 1,
   },
 
-  control: {
-    padding: 2,
+  filterBox: {
+    //flex:1,
+    //position: "-webkit-sticky", /* Safari */
+    position: "fixed",
+    right: "100px",
+    top: "200px",
+
+    height: "300px",
+    width: "200px",
   },
+
+  formControl: {
+    //padding: 2,
+  },
+
+  formLabel: {
+    backgroundColor: "#E5E5E5",
+    borderRadius: "25px",
+    paddingLeft: "0px",
+    paddingRight: "10px",
+    paddingBottom: "0px",
+    paddingTop: "0px",
+    fontSize: 14,
+    marginTop: "12px",
+  },
+
+  formCheckBox: {},
 });
 
 export default function Home() {
   const classes = useStyles();
   // eslint-disable-next-line
-  const [postState, setPostState] = useState<any[]>([]);
+  const [state, setState] = React.useState({
+    announcement: true,
+    hw: true,
+    project: true,
+    exam: true,
+  });
 
-  useEffect(() => {
-    console.log(postState);
-    getPosts("1") // classId is hardcoded for now
-      .then((res) => {
-        setPostState(res);
-      })
-      .catch((err) => console.error(err));
-  }, []); // TODO: Add loading indicator and put "refresh" into empty array
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const { announcement, hw, project, exam } = state;
 
   return (
-    <Container component="main" maxWidth="md">
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={6}
-        className={classes.root}
-        //style={{ width: "500px" }}
-      >
-        <Grid item>
-          <NewPost
-            onClick={(post: postModel) => {
-              addPost(post.userId, post.classId, post).then(
-                (resultingId: any) => {
-                  post.id = resultingId;
+    <Container component="main" maxWidth="md" className={classes.root}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Feed />
+
+        <Card className={classes.filterBox}>
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormLabel component="legend">Choose Categories</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checkedIcon={<CircleCheckedFilled />}
+                    icon={<CircleUnchecked />}
+                    checked={announcement}
+                    onChange={handleChange}
+                    name="announcement"
+                  />
                 }
-              );
-              setPostState([post, ...postState]);
-            }}
-          />
-        </Grid>
-        {postState.map((thisPost, index) => (
-          <Grid item key={thisPost.id} style={{ width: "500px" }}>
-            <FeedItem
-              id={thisPost.id}
-              postUserName={thisPost.postUserName}
-              postClassName={thisPost.postClassName}
-              postText={thisPost.postText}
-              timestamp={thisPost.timestamp}
-              edited={thisPost.edited}
-              userId={thisPost.userId}
-              classId={thisPost.classId}
-            />
-          </Grid>
-        ))}
-      </Grid>
+                label="Announcement"
+                className={classes.formLabel}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checkedIcon={<CircleCheckedFilled />}
+                    icon={<CircleUnchecked />}
+                    checked={hw}
+                    onChange={handleChange}
+                    name="hw"
+                  />
+                }
+                label="HW"
+                className={classes.formLabel}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checkedIcon={<CircleCheckedFilled />}
+                    icon={<CircleUnchecked />}
+                    checked={project}
+                    onChange={handleChange}
+                    name="project"
+                  />
+                }
+                label="Exam"
+                className={classes.formLabel}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checkedIcon={<CircleCheckedFilled />}
+                    icon={<CircleUnchecked />}
+                    checked={exam}
+                    onChange={handleChange}
+                    name="exam"
+                  />
+                }
+                label="Project"
+                className={classes.formLabel}
+              />
+            </FormGroup>
+          </FormControl>
+        </Card>
+      </div>
     </Container>
   );
 }
