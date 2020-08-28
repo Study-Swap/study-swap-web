a462e104f7b5f7e36d1fe86ef291da670b39ef98
cmodi <cmodi@umich.edu>
2020-08-28 12:02:32 -0400

    classes connect

--- src/views/Class.tsx
+++ src/views/Class.tsx
@@ -6,8 +6,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import FeedItem from "../components/FeedItem";
 import { postData } from "../DummyData/home";
 
+import { getPosts } from "../utils/firebaseUtils";
+
 import ClassCard from "../components/ClassCard";
 import ClassRoster from "../components/ClassRoster";
+import { postModel } from "../constants/Models";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,7 +20,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Class(props: any) {
   const classes = useStyles();
-  const [postState] = useState(postData);
+  const [postState, setPostState] = useState<postModel[]>([]);
+
+  React.useEffect(() => {
+    getPosts("1") // classId is hardcoded for now
+      .then((res) => {
+        setPostState(res);
+      })
+      .catch((err) => console.error(err));
+  }, []); // TODO: Add loading indicator and put "refresh" into empty array
 
   return (
     <Container component="main" maxWidth="md">
@@ -33,29 +44,35 @@ export default function Class(props: any) {
         <br />
         <div style={{ display: "flex", flexDirection: "row" }}>
           <div style={{ width: 470, marginRight: 10 }}>
-            {postState.map((thisPost, index) => {
-              var isLiked;
-              thisPost.likedBy.indexOf(props.userId) !== -1
-                ? (isLiked = true)
-                : (isLiked = false);
-              return (
-                <React.Fragment key={index}>
-                  <FeedItem
-                    postCategory={thisPost.postCategory}
-                    postUserName={thisPost.postUserName}
-                    postClassName={thisPost.postClassName}
-                    postText={thisPost.postText}
-                    timestamp={thisPost.timestamp}
-                    edited={thisPost.edited}
-                    userId={thisPost.userId}
-                    classId={thisPost.classId}
-                    likedBy={thisPost.likedBy}
-                    isLiked={isLiked}
-                  />
-                  <br />
-                </React.Fragment>
-              );
-            })}
+            {postState.length > 0 ? (
+              postState.map((thisPost: postModel, index) => {
+                var isLiked;
+                thisPost.likedBy.indexOf(props.userId) !== -1
+                  ? (isLiked = true)
+                  : (isLiked = false);
+                return (
+                  <React.Fragment key={index}>
+                    <FeedItem
+                      id={thisPost.id}
+                      postUserName={thisPost.postUserName}
+                      postClassName={thisPost.postClassName}
+                      postText={thisPost.postText}
+                      timestamp={thisPost.timestamp}
+                      postCategory={thisPost.postCategory}
+                      edited={thisPost.edited}
+                      userId={thisPost.userId}
+                      classId={thisPost.classId}
+                      likedBy={thisPost.likedBy}
+                      isLiked={isLiked}
+                      profilePic={thisPost.postUserProfilePic}
+                    />
+                    <br />
+                  </React.Fragment>
+                );
+              })
+            ) : (
+              <div />
+            )}
           </div>
           <div style={{ width: 380, marginLeft: 10 }}>
             <ClassRoster />
